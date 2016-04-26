ac376fa3971454af21b2cdef1480c5f6b3f4a709
Joseph Scott <email": null>
2016-04-26 15:17:25 -0500

    Add media player funcitonality

--- app/assets/javascripts/media-player.js
+++ app/assets/javascripts/media-player.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
   $('.media-play-btn').on('click', function(event) {
     event.preventDefault();
+    var $playBtn = $(this);
     $songClicked = document.getElementById("song"+ $(this).attr("song"));
 
     if ($songClicked.paused == true) {
