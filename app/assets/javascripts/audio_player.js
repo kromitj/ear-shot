768d1efa38018c746c224fa87dd8a046d52eeef3
Joseph Scott <email": null>
2016-04-25 19:45:21 -0500

    add jplayer partial

--- app/assets/javascripts/audio_player.js
+++ app/assets/javascripts/audio_player.js
@@ -2,7 +2,7 @@ $(document).ready(function()
             {
         var myCirclePlayer = new CirclePlayer("#jquery_jplayer_1",
         {
-            mp3: "MyMP3File.mp3"
+            mp3: "#{song.attachment}"
         }, {
                 supplied: "mp3",
                 cssSelectorAncestor: "#cp_container_1",
