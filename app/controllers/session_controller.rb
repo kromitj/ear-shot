df8b9239b501a13faf57a114c26000dd262da4da
byronbenjamin <byrongage@yahoo.com>
2016-04-27 16:33:00 -0500

    add splash page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -26,11 +26,11 @@ class SessionController < ApplicationController
     log_out
     redirect_to root_path
   end
-  #
-  # def destroy
-  #   session.delete
-  #   redirect_to root_path
-  # end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
 
 private
 
