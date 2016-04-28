9ffdd55e99749d787a4c18e034b78b7124124a45
Connor Reaumond <creaumond@gmail.com>
2016-04-27 19:39:32 -0500

    fix yaboi's deletions'

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -31,6 +31,34 @@ class RequestsController < ApplicationController
     render :json => nearby_songs_array, :include {:artist => {:only => [:name, :hometown, :bio, :profile_picture]}, :comments => {:only => [:content]}, :favorites, :listens}
 git
   end
+  def near
+    @songs = Song.all
+    @artists = Artist.all
+    p @songs
+    return_array =[]
+
+    @song_list = []
+    @user_loc = [params[:lat], params[:long]]
+    @test_loc =[40.7484, 73.9857]
+
+    @songs.each do |song|
+      array = []
+      array.push(song.locations.first.lat)
+      array.push(song.locations.first.long)
+      if distance_between(@user_loc, array) < song.locations.first.radius*1610
+        return_array.push(song)
+      end
+    end
+    print return_array
+    render :json => return_array, :include => {:artist => {:only => [:name, :hometown, :bio, :profile_picture]}, :comments => {:only => [:content]}}, :include => :locations
+
+
+
+    result = distance_between(@user_loc, @test_loc)
+    puts result
+
+
+  end
 
 private
 
