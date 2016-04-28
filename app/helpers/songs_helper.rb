4be52ecebf63304e3c15e4414a1c23c3de835384
Connor Reaumond <creaumond@gmail.com>
2016-04-28 08:25:42 -0500

    fix user show page

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,4 +2,8 @@ module SongsHelper
   def remote_upload(song)
     S3_BUCKET.objects.create(song, @file_data.tempfile)
   end
+
+  def last_5(user_id)
+    Listen.where(user_id: user_id).last(10)
+  end
 end
